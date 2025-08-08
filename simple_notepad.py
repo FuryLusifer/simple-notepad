a7b3f1ce2189ab9862f33c7c80965429b512f5fd
FuryLusifer <piyushkayastha2@gmail.com>
2025-08-08 15:19:35 +0545

    Transformed code adhering to OOP

--- simple_notepad.py
+++ simple_notepad.py
@@ -3,55 +3,23 @@
 
 import os
 
-def show_welcome_message():
-    print("\n********** Welcome to my Notepad **********\n")
 
-def show_menu():
-    print("------- Menu --------")
-    print("1. Create a new note")
-    print("2. Read a note")
-    print("3. Append to a note")
-    print("4. Delete a note")
-    print("5. Exit")
+class SimpleNotepad:
+    def show_welcome_message(self):
+        print("\n********** Welcome to my Notepad **********\n")
 
-def create_note():
-    filename = input("Enter the filename to create: ").strip()
-    if filename:
-        print("Start typing your note (type SAVE to finish): ")
-        content = ''
+    def show_menu(self):
+        print("------- Menu --------")
+        print("1. Create a new note")
+        print("2. Read a note")
+        print("3. Append to a note")
+        print("4. Delete a note")
+        print("5. Exit")
 
-        while True:
-            user_input = input()
-            if user_input.lower() == "save":
-                break
-            content += user_input + "\n"
-
-        with open(filename, 'w') as file:
-            file.write(content)
-            print(f"Your note has been saved in file '{filename}'.")
-    else:
-        print("Filename cannot be empty")
-
-def read_note():
-    filename = input("Enter the filename to read: ").strip()
-    if os.path.exists(filename):
-        with open(filename, 'r') as file:
-            print(f"------ Contents of {filename} ------")
-            content = file.read()
-            if not content:
-                print("The note is empty.")
-            else:
-                print(content)
-
-    else:
-        print("File does not exist.")
-
-def append_note():
-    filename = input("Enter the filename to append: ").strip()
-
-    if os.path.exists(filename):
-        with open(filename, "a") as file:
-            print("Start adding to your note (type SAVE to finish.)")
+    def create_note(self):
+        filename = input("Enter the filename to create: ").strip()
+        if filename:
+            print("Start typing your note (type SAVE to finish): ")
             content = ''
 
             while True:
@@ -60,56 +28,91 @@ def append_note():
                     break
                 content += user_input + "\n"
 
-            file.write(content)
-            print("Your note has been appended.")
+            with open(filename, 'w') as file:
+                file.write(content)
+                print(f"Your note has been saved in file '{filename}'.")
+        else:
+            print("Filename cannot be empty")
 
-    else:
-        print("File does not exist.")
+    def read_note(self):
+        filename = input("Enter the filename to read: ").strip()
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                print(f"------ Contents of {filename} ------")
+                content = file.read()
+                if not content:
+                    print("The note is empty.")
+                else:
+                    print(content)
 
-def delete_note():
-    filename = input("Enter the filename to delete: ").strip()
-
-    if os.path.exists(filename):
-        try:
-            os.remove(filename)
-            print(f"'{filename}' has been deleted successfully.")
-        
-        except FileNotFoundError:
+        else:
             print("File does not exist.")
-            
-        except PermissionError:
-            print("Permission denied: Cannot delete this file.")
-            
-        except Exception as e:
-            print(f"Unexpected error: {e}")
 
-    else:
-        print("File does not exist.")
-    
-def close_program():
-    print("See you next time.")
-    exit()
+    def append_note(self):
+        filename = input("Enter the filename to append: ").strip()
+
+        if os.path.exists(filename):
+            with open(filename, "a") as file:
+                print("Start adding to your note (type SAVE to finish.)")
+                content = ''
+
+                while True:
+                    user_input = input()
+                    if user_input.lower() == "save":
+                        break
+                    content += user_input + "\n"
+
+                file.write(content)
+                print("Your note has been appended.")
+
+        else:
+            print("File does not exist.")
+
+    def delete_note(self):
+        filename = input("Enter the filename to delete: ").strip()
+
+        if os.path.exists(filename):
+            try:
+                os.remove(filename)
+                print(f"'{filename}' has been deleted successfully.")
+            
+            except FileNotFoundError:
+                print("File does not exist.")
+                
+            except PermissionError:
+                print("Permission denied: Cannot delete this file.")
+                
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+
+        else:
+            print("File does not exist.")
+        
+    def close_program(self):
+        print("See you next time.")
+        exit()
+
+my_notepad = SimpleNotepad()
 
 while True:
-    show_welcome_message()
-    show_menu()
-
+    my_notepad.show_welcome_message()
+    my_notepad.show_menu()
     choice = input("Enter your choice: ")
 
-    if choice == '5':
-        close_program()
-
-    elif choice == '1':
-        create_note()
+    if choice == '1':
+        my_notepad.create_note()
 
     elif choice == '2':
-        read_note()   
+        my_notepad.read_note()   
 
     elif choice == '3':
-        append_note()
+        my_notepad.append_note()
     
     elif choice == '4':
-        delete_note()
+        my_notepad.delete_note()
+
+    elif choice == '5':
+        my_notepad.close_program()
            
     else:
         print("Wrong input! Choose again!!")
